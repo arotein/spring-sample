c4c4fb432be010d1cfaba7f46360393949e77fad
CHANU-LAPTOP\9cksq <9cksqhdel3@naver.com>
2023-05-30 01:42:53 +0900

    S3 application.yml 설정, put, get API 테스트

--- src/main/java/com/example/demo/s3/controller/S3Controller.java
+++ src/main/java/com/example/demo/s3/controller/S3Controller.java
@@ -3,8 +3,8 @@ package com.example.demo.s3.controller;
 import com.example.demo.s3.service.S3Service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @RequestMapping("/api/s3")
@@ -12,4 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class S3Controller {
     private final S3Service service;
+
+    @PostMapping("/img")
+    public String imgUpload(@RequestPart MultipartFile file) {
+        service.upload(file);
+        return "성공";
+    }
+
+    @GetMapping("/img/{key}")
+    public String imgGet(@PathVariable String key) {
+        service.get(key);
+        return "성공";
+    }
 }
--- src/main/java/com/example/demo/s3/service/S3Service.java
+++ src/main/java/com/example/demo/s3/service/S3Service.java
@@ -1,11 +1,46 @@
 package com.example.demo.s3.service;
 
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.S3Object;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.InputStream;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class S3Service {
+    private final AmazonS3Client amazonS3Client;
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @SneakyThrows
+    public void get(String key) {
+        // key는 파일의 path를 의미
+        S3Object object = amazonS3Client.getObject(bucket, key);
+        InputStream delegateStream = object.getObjectContent().getDelegateStream();
+        Files.copy(delegateStream, Paths.get("C:/sampleImg", object.getKey()), StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    public void upload(MultipartFile file) {
+        try {
+            ObjectMetadata objectMetadata = new ObjectMetadata();
+            objectMetadata.setContentType(file.getContentType());
+            objectMetadata.setContentLength(file.getSize());
+
+            amazonS3Client.putObject(bucket, "sample/" + file.getOriginalFilename(), file.getInputStream(), objectMetadata);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
